6d003298b69c69e145d5989458605a38995da80f
이준희 <junh0328@naver.com>
2021-01-24 14:39:14 +0900

    테이블 명 강제 변환

--- prepare/backend/models/image.js
+++ prepare/backend/models/image.js
@@ -10,6 +10,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       charset: 'utf8',
       collate: 'utf8_general_ci', // 한글 저장(utf8)
+      tableName: 'images',
     }
   );
   Image.associate = (db) => {
--- prepare/backend/models/post.js
+++ prepare/backend/models/post.js
@@ -11,6 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     {
       charset: 'utf8mb4',
       collate: 'utf8mb4_general_ci', // 한글 저장(utf8) + 이모티콘 저장(mb4)
+      tableName: 'posts',
     }
   );
   Post.associate = (db) => {
